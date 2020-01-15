9a89c2b708c3010a662bc19b2ec2610dd8bd7eb2
oleng <oleng@users.noreply.github.com>
2020-01-16 01:10:17 +0700

    changed to multistaged, split pgcli deps on ones dependent on gcc, renamed to docker-entrypoint.sh (should be standard), fixed many

--- pgcli/docker-entrypoint.sh
+++ pgcli/docker-entrypoint.sh
@@ -6,14 +6,18 @@
 db_url=$1
 
 usage() {
-    echo "3 ways to use this container:"
-    echo ""
-    echo "1. Directly pass the database url:"
-    echo "   docker run -it --rm dencold/pgcli postgres://DBUSER:DBPASS@DBHOST:DBPORT"
-    echo "2. Pass an environment variable via docker's '-e' argument"
-    echo "   docker run -it --rm -e DB_URL=postgres://DBUSER:DBPASS@DBHOST:DBPORT dencold/pgcli"
-    echo "3. Automatically connect via a linked postgres container (my favorite):"
-    echo "   docker run -it --rm --link YOURCONTAINER:postgres dencold/pgcli"
+    echo 'To use this container:'
+    echo ''
+    echo '1. Directly pass the database url:'
+    echo '   docker run -it --rm <repo>/pgcli:<tag> postgres://<user>:<passwd>@<host>:<port>'
+    echo ''
+    echo '2. Pass an environment variable via `-e` argument'
+    echo '   docker run -it --rm -e DB_URL=postgres://<user>:<passwd>@<host>:<port> <repo>/pgcli:<tag>'
+    echo ''
+    echo '3. Automatically connect via a linked postgres container:'
+    echo '   docker run -it --rm --link <running-container-name>:<db-name> <repo>/pgcli:<tag>'
+    echo ''
+    echo 'Exiting.'
 }
 
 if [ -n "$db_url" ]; then
@@ -23,11 +27,14 @@ elif [ -n "$DB_URL" ]; then
     # next, if a DB_URL environment variable is set, use that
     pgcli "$DB_URL"
 elif [ -n "$POSTGRES_PORT_5432_TCP_ADDR" ]; then
+    if [ -z "$POSTGRES_ENV_POSTGRES_USER" ]; then
+        POSTGRES_ENV_POSTGRES_USER='postgres'
+    fi
     # if nothing is set, we try to construct a db_url from the env vars that docker
     # automatically sets for the postgres container
     pgcli postgres://$POSTGRES_ENV_POSTGRES_USER:$POSTGRES_ENV_POSTGRES_PASSWORD@$POSTGRES_PORT_5432_TCP_ADDR:$POSTGRES_PORT_5432_TCP_PORT
 else
-    echo "Database URL not provided, please try again."
-    echo ""
+    echo 'Database URL not provided, please try again.'
+    echo ''
     usage
 fi
